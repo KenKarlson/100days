69985e02035013f086cbecb63770d456062e94f5
Гусь Гитарист <kenkarlson@mail.ru>
2025-02-27 12:09:44 +0300

    Add task 2

--- day2/index.js
+++ day2/index.js
@@ -5,3 +5,26 @@ function calculateDiscount(price, discountPercent){
 }
 let x = calculateDiscount(1000, 20);
 console.log(x.toFixed(2));
+
+const listOfProducts = [
+  { name: 'Яблоки', category: 'Фрукты', price: 120, quantity: 2 },
+  { name: 'Молоко', category: 'Молочные продукты', price: 80, quantity: 1 },
+  { name: 'Бананы', category: 'Фрукты', price: 60, quantity: 3 },
+  { name: 'Сыр', category: 'Молочные продукты', price: 300, quantity: 0.5 }
+];
+let arrOne =[];
+let arrTwo =[];
+for (const element of listOfProducts) {
+  if(element.category === 'Фрукты'){
+    arrOne.push(element);
+  }else{
+    arrTwo.push(element);
+  }
+};
+
+for(const elem of arrOne){
+  console.log(`${elem.name} ${elem.price} * ${elem.quantity} = ${elem.price * elem.quantity}`);
+}
+for(const elem of arrTwo){
+  console.log(`${elem.name} ${elem.price} * ${elem.quantity} = ${elem.price * elem.quantity}`);
+}
