699eb69f272209e81353e66df647e96f3052052a
Ken Karlson <javarecords@mail.ru>
2025-03-02 17:36:46 +0300

    Add case

--- day5/index.js
+++ day5/index.js
@@ -1,6 +1,23 @@
 function calculate(a, b, operator) {
   result = "";
-  return result;
+  if (true) {
+    switch (operator) {
+      case "+":
+        result = a + b;
+        break;
+      case "-":
+        result = a - b;
+        break;
+      case "*":
+        result = a * b;
+        break;
+      case "/":
+        result = a / b;
+        break;
+      default:
+        result = 'Нет такой валюты "Спасибо"';
+    }
+    return result;
+  }
 }
-
-console.log(calculate(5, 5, "+"));
+console.log(calculate(2, 5, "www"));
