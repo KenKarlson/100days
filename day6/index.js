d7977bb1decd9717d12fcc824223983a53c0cf14
Гусь Гитарист <kenkarlson@mail.ru>
2025-03-03 09:10:31 +0300

    Add reset & border

--- day6/index.js
+++ day6/index.js
@@ -1,19 +1,44 @@
-function createCounter(value = 0){
+function createCounter(value = 0) {
   let count = value;
   return {
-    increment(){
+    increment() {
       count++;
     },
-    decrement(){
+    decrement() {
       count--;
     },
-    getValue(){
+    getValue() {
       return count;
-    }
-  }
+    },
+  };
 }
+function createCounterUniverse(value = 0, step = 1) {
+  let count = value;
+  let initial = value;
+  let minValue = -Infinity;
+  let maxValue = Infinity;
 
+  function checkBorders() {
+    count < minValue ? (count = minValue) : count > maxValue ? (count = maxValue) : null;
+  }
 
+  return {
+    increment() {
+      count += step;
+      checkBorders();
+    },
+    decrement() {
+      count -= step;
+      checkBorders();
+    },
+    getValue() {
+      return count;
+    },
+    reset() {
+      count = initial;
+    },
+  };
+}
 
 let counter = createCounter(10);
 counter.increment();
@@ -22,3 +47,8 @@ console.log(counter.getValue()); //Magic
 let counter2 = createCounter(-10);
 counter.increment();
 console.log(counter2.getValue()); //Magic
+
+let testCounter = createCounterUniverse(10, 3);
+testCounter.decrement();
+console.log(testCounter.getValue()); //Magic
+
