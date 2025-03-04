0a93eda25226a1d299287bede73346b228b3504d
Ken Karlson <javarecords@mail.ru>
2025-03-04 19:51:13 +0300

    add vue to todo)

---
+++ day7/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <h1 class="title">{{ title }}</h1>
+      <div>
+        Осталось сделать задач: <span class="counter">{{ counter }}</span>
+      </div>
+      <div class="list">
+        <div class="item done">
+          <input type="checkbox" checked />
+          Развернуть окружение в Codepen
+        </div>
+        <div class="item">
+          <input type="checkbox" />
+          Пройти курс по Vue
+        </div>
+        <div class="item">
+          <input type="checkbox" />
+          Сделать интернет-магазин на Vue
+        </div>
+      </div>
+      <div class="form">
+        <input :placeholder="placeholder" />
+        <button type="button">Добавить</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+  data() {
+    return {
+      counter: 3,
+      title: "Список задач",
+      placeholder: "Добавить задачу",
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+</style>
