1ec0617eaa8e8e2dafe1b2a02d78e7c7520141d9
Ken Karlson <javarecords@mail.ru>
2025-03-15 18:57:14 +0300

    add btn fix

--- day16/assets/js/index.js
+++ day16/assets/js/index.js
@@ -1,8 +1,18 @@
 const playBtn = document.getElementById("play-button");
 const bgAudio = document.getElementById("background-audio");
+const playImg = document.querySelector(".playImg");
+
+let isPlaying = false;
 
 playBtn.addEventListener("click", () => {
-  bgAudio.play();
+  if (isPlaying) {
+    bgAudio.pause(); // Остановить воспроизведение
+    playImg.src = "./assets/img/play24.png"; // Изменить на иконку "Play"
+  } else {
+    bgAudio.play(); // Начать воспроизведение
+    playImg.src = "./assets/img/pause24.png"; // Изменить на иконку "Pause"
+  }
+  isPlaying = !isPlaying; // Переключить состояние
 });
 
 //Переделать на стрелочную !!!!!!
