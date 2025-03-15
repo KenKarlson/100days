223a616f8e62355af2e02cde8c3fcc2eb64bfc5d
Ken Karlson <javarecords@mail.ru>
2025-03-15 03:32:35 +0300

    add coment)

--- day16/assets/js/index.js
+++ day16/assets/js/index.js
@@ -2,3 +2,6 @@ document.getElementById("play-button").addEventListener("click", function () {
   var audio = document.getElementById("background-audio");
   audio.play();
 });
+//Переделать на стрелочную !!!!!!
+//Кнопкак вкл и вык вук!! круглая и справа висит с р
+//Почини кнопку r на клаве и { ;)!
